3e984f029d32a68b610ebf0adb46c5dc963054bb
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-07-17 15:30:03 -0300

    feat: git pull main

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, Button, Image } from "react-native";
 import { logo } from "../../assets/images";
+import { CardService } from "../../components/cardService";
 import { Header } from "../../components/header";
 
 import { styles } from './style';
@@ -9,9 +10,10 @@ export function Home({ navigation }: any) {
     return (
         <>
             <Header url={'www.google.com'} />
+            <CardService />
 
             <View style={styles.container}>
-                <Button title="aperte=me" onPress={() => navigation.navigate('Login')} />
+                {/* <Button title="aperte=me" onPress={() => navigation.navigate('Login')} /> */}
 
             </View>
         </>
