ebce8935500ae1f4e136174a053d0ba3a9794391
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-07-18 09:44:21 -0300

    feat: add modal in newCalled

---
+++ src/components/modalAdd/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Image, TextInput, View, ImageSourcePropType, Text } from "react-native";
+import { styles } from "./style";
+import { EmailIcon, EmailIconPng } from "../../assets/icons";
+import { SvgUri } from 'react-native-svg'
+
+
+export function ModalAdd() {
+    return (
+        <View style={styles.container}>
+
+
+        </View>
+
+
+    )
+}
